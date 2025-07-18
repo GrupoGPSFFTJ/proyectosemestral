19729504f0dd7c5f9830a901ab04efa06219cb63
Johnson Valenzuela <johnsondavisv4@hotmail.com>
2025-07-18 19:34:16 -0400

    Update service URLs in production environment with specific ports

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
     production: true,
     sessionDuration: '2h',
     services: {
-        core: 'http://core-service.proyecto-gps.svc.cluster.local',
-        clinical: 'http://clinical-service.proyecto-gps.svc.cluster.local',
-        nutrition: 'http://nutrition-service.proyecto-gps.svc.cluster.local',
-        odonto: 'http://odonto-service.proyecto-gps.svc.cluster.local',
-        patient: 'http://patient-service.proyecto-gps.svc.cluster.local',
-        pharmacy: 'http://pharmacy-service.proyecto-gps.svc.cluster.local',
-        vaccination: 'http://vaccination-service.proyecto-gps.svc.cluster.local'
+        core: 'http://core-service.proyecto-gps.svc.cluster.local:3001',
+        clinical: 'http://clinical-service.proyecto-gps.svc.cluster.local:3002',
+        nutrition: 'http://nutrition-service.proyecto-gps.svc.cluster.local:3003',
+        odonto: 'http://odonto-service.proyecto-gps.svc.cluster.local:3004',
+        patient: 'http://patient-service.proyecto-gps.svc.cluster.local:3005',
+        pharmacy: 'http://pharmacy-service.proyecto-gps.svc.cluster.local:3006',
+        vaccination: 'http://vaccination-service.proyecto-gps.svc.cluster.local:3007'
     }
 };
