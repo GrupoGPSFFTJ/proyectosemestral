04ddbb43a6cc75e6e4bb3c863d5e28e8048199ce
Johnson Valenzuela <johnsondavisv4@hotmail.com>
2025-07-18 20:40:41 -0400

    Add reverse proxy configuration for backend microservices and update API service paths

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
     production: true,
     sessionDuration: '2h',
     services: {
-        core: 'http://core-service.proyecto-gps.svc.cluster.local:3001',
-        clinical: 'http://clinical-service.proyecto-gps.svc.cluster.local:3002',
-        nutrition: 'http://nutrition-service.proyecto-gps.svc.cluster.local:3003',
-        odonto: 'http://odonto-service.proyecto-gps.svc.cluster.local:3004',
-        patient: 'http://patient-service.proyecto-gps.svc.cluster.local:3005',
-        pharmacy: 'http://pharmacy-service.proyecto-gps.svc.cluster.local:3006',
-        vaccination: 'http://vaccination-service.proyecto-gps.svc.cluster.local:3007'
+        core: '/api/core',
+        clinical: '/api/clinical',
+        nutrition: '/api/nutrition',
+        odonto: '/api/odonto',
+        patient: '/api/patient',
+        pharmacy: '/api/pharmacy',
+        vaccination: '/api/vaccination'
     }
 };
